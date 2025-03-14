68e097fd6dd03da803eff55d50c40b18e5c18450
Vansh-Narang <vanshnarang49906@gmail.com>
2025-03-14 12:18:21 +0530

    Added all tests

--- Frontend/app/src/Pages/RegisterPage.jsx
+++ Frontend/app/src/Pages/RegisterPage.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
-import toast, { Toaster } from 'react-hot-toast';
+import { toast, Toaster } from "react-hot-toast";
+
 import "./RegisterPage.css";
 
 const API_URL = "http://localhost:8000/api/users/register";
